e4103e375e0db839b365de02e6251265847f3c7c
Martin Corredor <dev.martincorredor@gmail.com>
2022-03-18 11:25:06 -0500

    Add some components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import SearchPage from './SearchPage';
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/search" element={<SearchPage></SearchPage>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SearchPage.css'
+
+
+const SearchPage = () => {
+  return (
+    <div className='searchPage'>
+        <div className='searchPage_info'>
+            <p>62 stays . 26 august to 30 august . 2 guest</p>
+            <h1>Stays nearby</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default SearchPage
